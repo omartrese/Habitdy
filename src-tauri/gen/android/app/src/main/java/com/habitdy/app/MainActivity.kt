c7d9e425b71ca0b80d75f44a7a5f87c0baf0b0d6
omartrese <omarlayachi13@gmail.com>
2025-06-16 19:13:47 +0200

    init project

---
+++ src-tauri/gen/android/app/src/main/java/com/habitdy/app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.habitdy.app
+
+class MainActivity : TauriActivity()
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.css";
+
+  import StepIndicator from '../lib/components/StepIndicator.svelte';
+  import Step1 from '../lib/components/Step1.svelte';
+  import Step2 from '../lib/components/Step2.svelte';
+  import Step3 from '../lib/components/Step3.svelte';
+  import Step4 from '../lib/components/Step4.svelte';
+  import ProfileResult from "../lib/components/ProfileResult.svelte";
+</script>
+
+<main>
+  
+</main>
